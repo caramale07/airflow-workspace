88598a1b77d82fb3b274d3fd75ca3233f74de9af
Fatulla V. Bashirov <fatulla.bashirov@prodata.az>
2025-01-14 17:39:58 +0400

    all

--- data/dags/example_dag.py
+++ data/dags/example_dag.py
@@ -1,23 +1,35 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
 
-def print_hello():
-    print("Hello, Airflow!")
-
+# Default arguments
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
+# DAG definition
 with DAG(
-    'example_dag',
+    dag_id='example_dag',
     default_args=default_args,
-    schedule_interval='@daily',
+    description='A simple example DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2025, 1, 1),
     catchup=False,
+    tags=['example'],
 ) as dag:
-    task = PythonOperator(
-        task_id='hello_task',
-        python_callable=print_hello,
+    task1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
     )
+
+    task2 = BashOperator(
+        task_id='say_hello',
+        bash_command='echo "Hello, Airflow!"',
+    )
+
+    task1 >> task2
