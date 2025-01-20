9c9479c19be8c29ba02d2ac8c99c09820960d979
Fatulla V. Bashirov <fatulla2004@gmail.com>
2025-01-21 00:51:19 +0400

    3minutes baby

--- helm-command.sh
+++ helm-command.sh
@@ -1,6 +1,6 @@
 helm install airflow apache-airflow/airflow --namespace airflow --set executor=CeleryExecutor --set workers.replicas=2  --set flower.enabled=true --set postgresql.enabled=true --set redis.enabled=true --set dags.persistence.enabled=true --set logs.persistence.enabled=true
 
-helm install airflow apache-airflow/airflow --namespace airflow --create-namespace -f values.yaml
+helm install airflow apache-airflow/airflow --namespace airflow-kubernetes --create-namespace -f airflow-kubernetes.yaml
 
 helm upgrade airflow apache-airflow/airflow --namespace airflow --create-namespace -f values.yaml
 
