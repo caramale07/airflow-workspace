ec5b69c6eadbe8fe8c93325fe9e8c465749f048e
Fatulla V. Bashirov <fatulla.bashirov@prodata.az>
2025-01-20 11:04:00 +0400

    stream

--- data/dags/transform_parquet_test.py
+++ data/dags/transform_parquet_test.py
@@ -14,12 +14,11 @@ url = "https://fatulla.codage.az/data/sales_transactions_3m.parquet"
 def transform_parquet():
 
     local_path = "/tmp/sales_transactions.parquet"
-    response = requests.get(url, stream=True)
-    response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+    response = requests.get(url)
+    response.raise_for_status()  
     
     with open(local_path, "wb") as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
+        file.write(response.content)
             
     # Read the Parquet file
     df = pd.read_parquet(local_path)
