7c3545f925c1046e05e2cac610af3add34793af3
Fatulla V. Bashirov <fatulla2004@gmail.com>
2025-01-31 16:37:33 +0400

    nothing

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ import pandas as pd
 import requests
 import io
 import pyarrow.parquet as pq
+import pyarrow as pa
 
 
+# test data
+data = {
+    'name': ['John', 'Anna', 'Peter', 'Linda'],
+    'age': [24, 13, 53, 33],
+    'city': ['New York', 'Paris', 'Berlin', 'London']
+}
 
-# Paths
-url = "https://fatulla.codage.az/data/sales_transactions_2m.parquet"
-response = requests.get(url)
-response.raise_for_status()  # Ensure the request was successful
+df = pd.DataFrame(data)
 
-# Print the first few bytes of the response to check its content
-print(response.content[:100])
+# write to parquet
+table = pa.Table.from_pandas(df)
+pq.write_table(table, 'data.parquet')
+
